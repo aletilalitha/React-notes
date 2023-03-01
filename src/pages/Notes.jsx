ee4e70098cb5acfe322be070e45b29a266362915
aletilalitha <aletilalitha@gmail.com>
2023-03-01 19:36:06 +0100

    changes

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -46,7 +46,7 @@ const Notes = ({ notes }) => {
       </header>
       <div className="notes__container">
         {filteredNotes.length == 0 && (
-          <p className="empty__notes">Note note found</p>
+          <p className="empty__notes">Note not found</p>
         )}
         {filteredNotes.map((note) => (
           <NoteItem key={note.id} note={note} />
